59bc8ec74807b6ad1477961f0102ed4ec5281ba3
soniavg <54545515+soniavg@users.noreply.github.com>
2019-09-01 18:10:32 +0200

    Update um.sql

--- database/um.sql
+++ database/um.sql
@@ -1,30 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Inang: localhost
--- Waktu pembuatan: 28 Sep 2014 pada 07.10
--- Versi Server: 5.5.24-log
--- Versi PHP: 5.4.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Basis data: `lr`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `groups`
---
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,9 +10,7 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
---
--- Dumping data untuk tabel `groups`
---
+
 
 INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
 (1, 'Standard user', ''),
@@ -43,9 +18,7 @@ INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
 
 -- --------------------------------------------------------
 
---
--- Struktur dari tabel `users`
---
+
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -60,9 +33,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- --------------------------------------------------------
 
---
--- Struktur dari tabel `users_session`
---
+
 
 CREATE TABLE IF NOT EXISTS `users_session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -71,6 +42,4 @@ CREATE TABLE IF NOT EXISTS `users_session` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
